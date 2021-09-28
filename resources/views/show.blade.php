e00c108aa437776eb68b5457a8a728683fff6258
eiji-344 <eiji.344.eiji@gmail.com>
2021-09-28 16:24:55 +0900

    [add]基本操作画面

---
+++ resources/views/show.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Diaries</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <h1 class="title">
+            {{ $diary->title }}
+        </h1>
+        <p class="edit">[<a href="/diaries/{{ $diary->id }}/edit">edit</a>]</p>
+        <form action="/diaries/{{ $diary->id }}" id="form_delete" method="post">
+            @csrf
+            @method('delete')
+            <input type="submit" style="display:none">
+            <p class='delete'>[<span onclick="return deleteDiary(this);">delete</span>]</p>
+        </form>
+        <div class="content">
+            <div class="content__diary">
+                <h3>本文</h3>
+                <p>{{ $diary->body }}</p>
+                <p class='updated_at'>{{ $diary->updated_at }}</p>
+            </div>
+        </div>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+        <script>
+        function deleteDiary(e) {
+            'use script';
+            if (confirm('消去すると復元できません。\n本当に削除しますか？')) {
+                document.getElementById('form_delete').submit();
+            }
+        }
+        </script>
+    </body>
+</html>
